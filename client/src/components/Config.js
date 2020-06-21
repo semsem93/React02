5c563f0dae372061969981729abbabb27279fa97
semsem93 <rla00220@gmail.com>
2020-06-22 02:53:27 +0900

    add landing page and update popular image

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+//THE MOVIE API BASE URL
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/';
+export const API_KEY = '4d979c9740f92051e3ac8bb5f5528acf';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_URL } from '../../Config';
+import MainImage from './Sections/Mainimage';
 
 function LandingPage() {
+
+    const [movies, setmovies] = useState([])
+    const [mainmovieImage, setmainmovieImage] = useState(null)
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+            setmovies([response.results])
+            setmainmovieImage(response.results[0])
+        })
+    }, [])
+
+    // && 이거는 mainmovieImage가 null이 아닐 때 실행하라는 코드임
+
     return (
-        <>
-        <div className="app">
-            <FaCode style={{ fontSize: '4rem' }} /><br />
-            <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{ width: '100%', margin: '0'}}>
+            
+            {mainmovieImage &&
+                <MainImage 
+                image={`${IMAGE_URL}w1280${mainmovieImage.backdrop_path}`}
+                title={mainmovieImage.original_title}
+                text={mainmovieImage.overview}
+                />
+            }
+
+            <div style={{ width: '85%', margin: 'lrem auto'}}>
+
+                <h2>Movie bt latest</h2>
+                <hr/>
+
+                {/* movie Grid Cards*/}
+
+            </div>
+            <div style={{display: 'flex', justifyContent:'center'}}>
+                <button>Load More</button>
+            </div>
         </div>
-        <div style={{ float:'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
     )
 }
 
